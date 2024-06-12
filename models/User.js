c080b233e4777d4e9954f805fd6a180ff9213c3e
Mida Hasian <hasianmida@gmail.com>
2024-06-12 15:00:57 +0700

    initial commit

---
+++ models/User.js
@@ -0,0 +1,29 @@
+// models/User.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+  nik: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  tableName: 'Users',
+  timestamps: true
+});
+
+module.exports = User;
